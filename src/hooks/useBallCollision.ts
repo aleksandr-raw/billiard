307608f2184a5b26b3dc12f6fcd4d4b9d9f31290
aleksandr-raw <104529174+aleksandr-raw@users.noreply.github.com>
2024-03-16 16:51:35 +0400

    Updates

--- src/hooks/useBallCollision.ts
+++
@@ -1,9 +0,0 @@
-import {useEffect} from "react";
-import {checkBallCollision} from "../utils/utils";
-import {Ball} from "../objects/Ball";
-
-export const useBallCollision = (balls: Ball[]) => {
-    useEffect(() => {
-        checkBallCollision(balls);
-    }, [balls]);
-};
